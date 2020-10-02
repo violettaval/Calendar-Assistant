8b744ea9da1abb4302f16aa4a8815ab011f236fa
violettaval <70643274+violettaval@users.noreply.github.com>
2020-10-01 23:19:32 -0500

    debug the local storage

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function hourFill() {
         blocks.addClass('future');
         $(".time-block").append(blocks);
     }
-    else if (hour = 9) {
+    else if (hour === 9) {
         var ninth = $('#9');
         $("#9").addClass("present");
         $('#9').append(ninth);
@@ -30,41 +30,34 @@ function hourFill() {
             newID.addClass('future');
             $("#" + m).append(newID);
         }
-    }    
+    }
     $(".time-block").append(blocks);
-
 }
 
 //change colors
 hourFill();
 
-
 //save feature on buttons
 var saving = $('.saveBtn');
-saving.on('click', function(event){
+saving.on('click', function (event) {
     event.preventDefault();
-    console.log("click");
-    localStorage.setItem("data", JSON.stringify(data));
     var IDify = this.id.trim("b");
-    var formInput = document.querySelector("#"+IDify);
+    var formInput = $("#" + IDify);
     var data = {
         ID: IDify,
         text: formInput.textContent.trim()
-    } 
-    
+    };
+    console.log(data);
+    localStorage.setItem("data", JSON.stringify(data));
 });
 //append
 $('.saveBtn').append(saving);
 
 //grab local data
 var lastentries = JSON.parse(localStorage.getItem("data"));
-//give all current and future boxes forms
-
-
-
 
 //fill previous entries
-for(var l = 0;l< lastentries.length;l++){
+for (var l = 0; l < lastentries.length; l++) {
     var formOutput = document.querySelector(lastentries[l].ID);
     formOutput.textContent = lastentries[l].text;
     $(lastentries[l].ID).append(formOutput);
